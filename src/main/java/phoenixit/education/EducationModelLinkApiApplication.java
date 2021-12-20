2b48d3123ccfd9839ea5d9623aa9cacdf9708873
drewandrewd <andrewid10@gmail.com>
2021-12-20 18:18:30 +0500

    new

---
+++ src/main/java/phoenixit/education/EducationModelLinkApiApplication.java
@@ -0,0 +1,13 @@
+package phoenixit.education;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducationModelLinkApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EducationModelLinkApiApplication.class, args);
+	}
+
+}
